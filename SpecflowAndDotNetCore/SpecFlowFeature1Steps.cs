b7d0c74ab7f951ae264a730f1dcfa39011ea7252
Abhy.Kizhakkepat <Abhy.Kizhakkepat@tattsgroup.com>
2019-05-07 08:56:30 +1000

    Added new .NET Core Unit Test Project with SpecFlow.

---
+++ SpecflowAndDotNetCore/SpecFlowFeature1Steps.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowAndDotNetCore
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Assert.IsTrue(true);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Assert.IsTrue(true);
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
